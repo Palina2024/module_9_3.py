1bc5e155a8c566047837aa9506efc4700733881d
Palina <polina.sofina.20@mail.ru>
2024-12-26 19:24:19 +0500

    Add files via upload

---
+++ module_9_3.py
@@ -0,0 +1,14 @@
+first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютер']
+
+# 1. Генераторная сборка для разницы длин строк, если их длины не равны
+first_result = (len(f) - len(s) for f, s in zip(first, second) if len(f) != len(s))
+
+# 2. Генераторная сборка для сравнения длин строк в одинаковых позициях
+second_result = (len(first[i]) == len(second[i]) for i in range(len(first)))
+
+
+print("first_result:", list(first_result))
+print("second_result:", list(second_result))
+
+
